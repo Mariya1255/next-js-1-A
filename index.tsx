ddfda5e49aae7d6438f5ce3fdc3c402e48c0b121
Mariya1255 <162633244+Mariya1255@users.noreply.github.com>
2024-10-01 02:25:40 +0500

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNrtobD_olI0I8kMhyq0MqWXrHFZBaBc9M4w&usqp=CAU" width="900" alt="Sorry" />
+      </div>
+  )}
